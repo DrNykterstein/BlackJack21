aaf8917a6afc995c99ac7e444f7df5cab947bb76
Daniel Leota <46200971+DrNykterstein@users.noreply.github.com>
2021-01-07 00:18:44 -0400

    Lista de barajas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
-console.log("prueba");
+let baraja = [];
+const tipos = ['C','D','H','S'];
+const barajasEspeciales = ['A','J','Q','K'];
+
+const crearBaraja = () => {
+    for(let i = 2; i <= 10; i++){
+        for(let tipo of tipos){
+            baraja.push(i+tipo);
+        }
+    }
+
+    for(let tipo of tipos){
+        for(especial of barajasEspeciales){
+            baraja.push(especial+tipo);
+        }
+    }
+    
+    console.log(baraja);
+    baraja = _.shuffle(baraja);
+    console.log(baraja);
+    return baraja;
+}
+
+crearBaraja();
